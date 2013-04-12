93ebee066f1c296605351a3bff90c47e7f9d7c3f
Hoang Quan <hoang.van.quan@framgia.com>
2013-04-12 09:09:23 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myproject::Application.config.secret_token = '1c8c8d00db62cb2f824529ca223990f09a771f8b302429d1eb80fd66da7cf485d4f9045cb41895199442c4f8db95522f3c409f957b75aafb8b87532c24f68f32'
